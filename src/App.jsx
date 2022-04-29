fc0f1c7645c48237f649fcc6db15f098e13fe23f
parhamrt32 <92752702+parhamrt32@users.noreply.github.com>
2022-04-29 15:03:45 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
 }
 
 export default App;
